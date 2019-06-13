06345c8a5c0a67b4602deeb899912cf62335d424
wangxindong <znzd123456>
2019-06-13 16:56:18 +0800

    elk + sleuth 分布式日志

--- demo/src/main/java/com/dong/demo/DemoApplication.java
+++ demo/src/main/java/com/dong/demo/DemoApplication.java
@@ -3,11 +3,10 @@ package com.dong.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 @Slf4j
 @RestController
